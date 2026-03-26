a1249f85957df36e6c383375df6dee9295d7babe
shivanshdhyani-dev <shivansh.dhyani@lenskart.com>
2026-03-26 16:54:47 +0530

    Update socket.js

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000"|| "https://comm-mixer.onrender.com/";
 
 export function createMixerSocket() {
   return io(BACKEND_URL, {
